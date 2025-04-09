c088160385a3f2aebf31cd4bd592e0fab77758c3
chaeeun-nim <qlqldkdk0@gamil.com>
2025-04-09 22:17:06 +0900

    ✨ 백준문제풀이

--- workspace/boj/04/04.js
+++ workspace/boj/04/04.js
@@ -184,16 +184,47 @@
 1
 */
 
-const fs = require("fs");
-const fileData = fs.readFileSync(0).toString().trim().split("\n");
-const c = fileData.length;
-let max = fileData[0];
-let where = 0;
+function main() {
+  const data = getData();
+  // data에서 값을 꺼내서 문제 해결하는 코드 작성
+  let result = data[0][0];
+  let resultC = 1;
 
-for (let i = 0; i < c; i++) {
-  if (fileData[i] >= max) {
-    max = parseInt(fileData[i]);
-    where = parseInt(i + 1);
+  for (let i = 1; i < data.length; i++) {
+    if (data[i][0] >= result) {
+      result = data[i][0];
+      resultC = i + 1;
+    } else {
+      result = result;
+      resultC = resultC;
+    }
   }
+  console.log(result);
+  console.log(resultC);
 }
-console.log(max + "\n" + where);
+main();
+
+/**
+ * 표준 입력장치(콘솔)에서 두 줄로 입력된 줄당 en 건의 데이터를 읽어서 숫자로 변환한 후
+ * 객체에 a, b 속성으로 저장하여 반환한다.
+ * @returns {[]} 2차원 배열로 리턴하겠다!
+ *
+ */
+function getData() {
+  const fs = require("fs");
+  const fileData = fs.readFileSync(0).toString();
+  const arr = fileData.trim().split("\n");
+
+  const result = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    const row = arr[i];
+    const rowArr = row.split(" ");
+    for (let k = 0; k < rowArr.length; k++) {
+      rowArr[k] = isNaN(rowArr[k]) ? rowArr[k] : parseInt(rowArr[k]);
+    }
+    result.push(rowArr);
+  }
+
+  return result;
+}
--- workspace/boj/04/07.js
+++ workspace/boj/04/07.js
@@ -81,3 +81,51 @@ X대학 M교수님은 프로그래밍 수업을 맡고 있다.
 8
 */
 
+function main() {
+  const data = getData();
+  // data에서 값을 꺼내서 문제 해결하는 코드 작성
+  let student = data.flat(); // 입력값의 1차원 배열
+  let thrity = []; // 1~30까지의 배열
+  let a = []; // 콘솔로그 결과창
+
+  for (let i = 1; i <= 30; i++) {
+    thrity.push(i);
+  }
+  // console.log(student);
+  // console.log(thrity);
+
+  for (let i = 0; i < 30; i++) {
+    if (!student.includes(thrity[i])) {
+      a.push(thrity[i]);
+    }
+  }
+  a.sort((a, b) => a - b);
+  console.log(a[0]);
+  console.log(a[1]);
+}
+main();
+
+/**
+ * 표준 입력장치(콘솔)에서 두 줄로 입력된 줄당 en 건의 데이터를 읽어서 숫자로 변환한 후
+ * 객체에 a, b 속성으로 저장하여 반환한다.
+ * @returns {[]} 2차원 배열로 리턴하겠다!
+ *
+ */
+function getData() {
+  const fs = require("fs");
+  const fileData = fs.readFileSync(0).toString();
+  const arr = fileData.trim().split("\n");
+
+  const result = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    const row = arr[i];
+    const rowArr = row.split(" ");
+    for (let k = 0; k < rowArr.length; k++) {
+      rowArr[k] = isNaN(rowArr[k]) ? rowArr[k] : parseInt(rowArr[k]);
+    }
+    result.push(rowArr);
+  }
+
+  return result;
+}
