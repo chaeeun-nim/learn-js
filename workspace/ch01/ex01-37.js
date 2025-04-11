b4decbb9f77e637f8f23318a72d08ea5b5069d91
chaeeun-nim <qlqldkdk0@gamil.com>
2025-04-11 15:54:44 +0900

    ✨ 변수와 배열,객체

--- workspace/ch01/ex01-37.js
+++ workspace/ch01/ex01-37.js
@@ -2,5 +2,17 @@
 Call by Value와 Call by Reference 차이
 */
 
+function add10(data1, data2) {
+  data1 += 10;
+  data2[0] += 10;
+  console.log("함수내부 : " + data1, data2[0]);
+}
 
+let d1 = 80; // type = number
+let d2 = [80]; // type = array(object)
 
+console.log("함수호출 이전 : " + d1, d2[0]);
+
+add10(d1, d2);
+
+console.log("함수호출 이후 : " + d1, d2[0]);
--- workspace/ch01/ex01-38.js
+++ workspace/ch01/ex01-38.js
@@ -2,5 +2,21 @@
 전역 변수와 지역 변수
 */
 
+/**
+ * 지정한 두수의 최대값을 반환한다.
+ * @param {number} n1
+ * @param {number} n2
+ * @returns {number} - 최대값
+ */
+function max(n1, n2) {
+  let result = 0;
+  if (n1 > n2) {
+    result = n1;
+  } else {
+    result = n2;
+  }
+  return result;
+}
 
-
+console.log(max(10, 20)); // 20
+console.log(max(200, 30)); // 200
